17626e3587fbb4923fb917bdfb09c26ba693375b
JayChenFE <jaychenfe@foxmail.com>
2020-04-25 15:55:04 +0800

    修改28注释,完成30

--- 28.实现-str-str.java
+++ 28.实现-str-str.java
@@ -8,7 +8,8 @@
 class Solution {
     // 将 needle（目标字符串） 依次与字符串 haystack 长度为 needle 的子串比较，
     // 完全相同则返回子串的数组下标。
-    // 一个指针用于遍历，遍历次数为 haystack 多余长度 + 1；一个指针用于依次比较是否与 needle 每一个字符相同
+    // 一个指针用于遍历，遍历次数为 haystack 多余长度 + 1；
+    // 一个指针用于依次比较是否与 needle 每一个字符相同
 
     public int strStr(String haystack, String needle) {
         int hLen = haystack.length(), nLen = needle.length();
